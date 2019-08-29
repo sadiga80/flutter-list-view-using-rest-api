343f7872d2028b19f01e49ac9614aa4bb045089a
Santhosh Adiga <sadiga80@gmail.com>
2019-08-29 21:45:50 +0530

    Initial commit
    Created a listview with REST API

---
+++ flutter_list_view_using_rest_api/lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  List data;
+
+  Future<String> getData() async {
+    var response = await http.get(
+        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+        headers: {"Accept": "application/json"});
+
+    this.setState(() {
+      data = json.decode(response.body);
+    });
+    print(data[1]["title"]);
+
+    return "Success!";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Listview"),
+      ),
+      body: ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: EdgeInsets.all(8.0),
+            child: Card(
+                color: Colors.blue[50],
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      ListTile(
+                        title: Text(
+                          data[index]["title"],
+                          textAlign: TextAlign.justify,
+                        ),
+                        subtitle: Text(
+                        data[index]["body"],
+                        textAlign: TextAlign.justify,
+                      ),
+                      ),
+                    ],
+                  ),
+                )),
+          );
+        },
+      ),
+    );
+  }
+}
